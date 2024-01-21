87b31ba43ff08de8d043a5feb66074ab7d69acdb
qianjindexiaozu <2899094864@qq.com>
2024-01-21 20:05:31 +0800

    lab1 has down

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,13 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if(head == NULL) return 0;
+    node *tortoies = head;
+    node *hare = head;
+    while(hare != NULL && hare->next != NULL && hare->next->next != NULL){
+        hare = hare->next->next;
+        tortoies = tortoies->next;
+        if(hare == tortoies) return 1;
+    }
     return 0;
 }
