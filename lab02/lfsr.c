e576446fb2d3e3b79b9280f9e8fcb25ace9ad256
qianjindexiaozu <2899094864@qq.com>
2024-01-22 01:32:33 +0800

    lab02 has down

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,16 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t a, b, c, d, new;
+    uint16_t temp = *reg;
+    a = temp & 1;
+    b = (temp >> 2) & 1;
+    c = (temp >> 3) & 1;
+    d = (temp >> 5) & 1;
+    new = a ^ b ^ c ^ d;
+    temp = temp >> 1;
+    temp &= ~(1 << 15);
+    temp |= (new << 15);
+    *reg = temp;
 }
 
--- lab02/list.c
+++ lab02/list.c
@@ -4,18 +4,25 @@
 void append_node (node** head_ptr, int new_data) {
 	/* First lets allocate memory for the new node and initialize its attributes */
 	/* YOUR CODE HERE */
-
+	//We must transfer **head_ptr, because when the linklist is nonexistent, 
+	//the head is null, which means we will malloc a linklist in this function 
+	//groundlessly without return the address of the head.
+	//Another solution is to return the head pointer(return the address of head).
+	node *nextNodeptr = (node*) malloc(sizeof(node));
+	nextNodeptr->val = new_data;
 	/* If the list is empty, set the new node to be the head and return */
 	if (*head_ptr == NULL) {
 		/* YOUR CODE HERE */
+		*head_ptr = nextNodeptr;
 		return;
 	}
 	node* curr = *head_ptr;
-	while (/* YOUR CODE HERE */ != NULL) {
+	while (/* YOUR CODE HERE */curr->next != NULL) {
 		curr = curr->next;
 	}
 	/* Insert node at the end of the list */
 	/* YOUR CODE HERE */
+	curr->next = nextNodeptr;
 }
 
 /* Reverse a linked list in place (in other words, without creating a new list).
@@ -26,9 +33,13 @@ void reverse_list (node** head_ptr) {
 	node* next = NULL;
 	while (curr != NULL) {
 		/* INSERT CODE HERE */
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
 	}
 	/* Set the new head to be what originally was the last node in the list */
-	*head_ptr = /* INSERT CODE HERE */
+	*head_ptr = prev;/* INSERT CODE HERE */
 }
 
 
